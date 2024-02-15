49e0a967a073c0e4ceb54737e767d55e3e661df8
BAHTIYORdev <sheraliyevbahtiyor36@gmail.com>
2024-02-15 15:53:42 +0500

    first commit

---
+++ lib/ui/get_started.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class GetStarted extends StatelessWidget {
+  const GetStarted({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        width: size.width,
+        height: size.height,
+        color: Colors.deepOrange,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/get-started.png'),
+              const SizedBox(height: 30),
+              Container(
+                height: 50,
+                width: size.width * 0.7,
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                child: const Center(
+                  child: Text('Get Started'),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
